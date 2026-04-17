03eeea7dec74601cf7be9837e524a2616b9937ff
samiajahan15 <samiajahan.euphoric@gmail.com>
2026-04-17 16:29:00 +0600

    Create phonebook.cpp

---
+++ phonebook.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Contact
+{
+    string name;
+    string phone;
+};
+
+int main()
+{
+    vector<Contact> contacts;
+
+    cout << "Phonebook Management System Started Successfully!" << endl;
+
+    return 0;
+}
